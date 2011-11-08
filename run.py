9a4213b8d23ab02ef93e1a54a6984cd6e727e91c
muffl0n <muffl0n@raffts.net>
2011-11-08 10:44:23 +0100

    cleaned up

--- run.py
+++ run.py
@@ -1,2 +1,2 @@
 from webircbot import app
-app.run(debug=True)
+app.run(host='89.238.75.18',debug=True)
--- webircbot/__init__.py
+++ webircbot/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = '\x1bf\xa2\xf5\x81u;\xfa\xc8\x88?\xc7\x91\x99\x15k\xb4\xc5|Am\xe7\x9f1'
 
-import web.views
+import views
