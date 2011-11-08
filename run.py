d8b088e4093402af9533c7b33de850403b551cd8
muffl0n <muffl0n@raffts.net>
2011-11-08 11:14:00 +0100

    run now has argument for listen-ip

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
+import sys
 from webircbot import app
-app.run(host='89.238.75.18',debug=True)
+
+if (len(sys.argv) != 2):
+	print "Usage: python %s <listen-ip>" % (sys.argv[0])
+else:
+	ip = sys.argv[1]
+	app.run(host=ip,debug=True)
+
